8b90ff6373d95964dcd3d4a4f34776fb574e123c
Damien <damienmauchamp@gmail.com>
2022-10-14 20:52:55 +0200

    roundedLine

--- draw.js
+++ draw.js
@@ -33,6 +33,58 @@ const leftBar = (height, width, color) => {
 	return resultBarImage;
 }
 
+const roundedLineVertical = (width, height, color) => {
+	if (width > height) {
+		let tmp = width;
+		width = height;
+		height = tmp;
+	}
+	return roundedLine(width, height, color);
+}
+
+const roundedLineHorizontal = (width, height, color) => {
+	if (width < height) {
+		let tmp = width;
+		width = height;
+		height = tmp;
+	}
+	return roundedLine(width, height, color);
+}
+
+const roundedLine = (width, height, color) => {
+	const tempBarWidth = width !== width ? height : 5;
+	const tempBarHeight = height !== undefined ? height : 100;
+	
+	color = color || Color.white()
+	
+	let cornerWidth = Math.min(
+	  tempBarWidth,
+	  tempBarHeight
+	) / 2,
+		cornerHeight = cornerWidth;
+	
+	const draw = new DrawContext()
+	draw.opaque = false
+	draw.respectScreenScale = true
+	draw.size = new Size(tempBarWidth, tempBarHeight)
+  
+	const barPath = new Path()
+	const barHeight = tempBarHeight - 10
+	
+  //   barPath.addRoundedRect(new Rect(0, 5, tempBarWidth, barHeight), barHeight / 2, barHeight / 2)
+	barPath.addRoundedRect(new Rect(0, 5, tempBarWidth, barHeight), cornerWidth, cornerHeight)
+	
+	draw.addPath(barPath)
+	
+	draw.setFillColor(color)
+	draw.fillPath(color)
+	
+	return draw.getImage()
+  }
+
 module.exports = {
 	leftBar: leftBar,
+	roundedLineVertical: roundedLineVertical,
+	roundedLineHorizontal: roundedLineHorizontal,
+	roundedLine: roundedLine,
 }
